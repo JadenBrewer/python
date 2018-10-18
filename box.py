6394fe54d1a835fb66123970f5a2b7b16bb9e11c
JadenBrewer <jtbrew8@gmail.com>
2018-10-18 13:33:45 -0700

    Add files via upload

--- box.py
+++ box.py
@@ -1,12 +1,16 @@
 import turtle
 import random
 def spiral():
+	turtle.speed(0)
 	t = turtle
 	t.color('blue')
+	t.colormode(255)
 	x = 10
 	a = 91
 	# Draw the base
-	for i in range(150):
+	for j in range(150):
+		t.color(0,0,100+j)
+		t.width(1)
 		for i in range(4):
 			t.forward(x)
 			t.left(a)
